3c99ef8dd2ed0ba3a965cc920720ee5d7c79b21a
Luca Giozza <luca.giozza@gmail.com>
2021-07-06 14:58:41 +0200

    inizio header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+       <Header/>
+
+        <main>
+         <!-- <PrimaSezione/> -->
+       </main> 
+
+       <!-- <Footer/> -->
+        
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Header from './components/Header.vue';
+//  import PrimaSezione from './components/PrimaSezione.vue';
+//  import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     Header,
+    //  PrimaSezione,
+    //  Footer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style> 
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <a href="#">
+
+          <img src="@/assets/dc-logo.png" alt="dc-logo">
+      </a>
+
+      <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+      </ul>
+  </header>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
